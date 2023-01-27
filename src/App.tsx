81be3da5f35789a761476739e982754a9eb11dfe
Nemanja Malesija <malesija13@gmail.com>
2023-01-27 18:05:21 +0100

    setup reducer and initial state

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useReducer } from 'react';
+import reducer from './reducer';
+import { useQuery, QueryFunctionContext } from '@tanstack/react-query';
 
 import './App.css';
 
+export type APIproducts = {
+  id: string;
+  title: string;
+  price: number;
+  description?: string;
+  category: string;
+  image: string;
+  rating: {
+    rate: number;
+    count: number;
+  };
+}[];
+
+const initialState: APIproducts = [
+  {
+    id: '',
+    title: '',
+    price: 0,
+    category: '',
+    image: '',
+    rating: {
+      rate: 0,
+      count: 0,
+    },
+  },
+];
+
 function App() {
-  return <div className="App"></div>;
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return <div className="App">const</div>;
 }
 
 export default App;
---
+++ src/reducer.ts
@@ -0,0 +1,7 @@
+import { APIproducts } from './App';
+
+const reducer = (state: APIproducts, action: any) => {
+  return state;
+};
+
+export default reducer;
